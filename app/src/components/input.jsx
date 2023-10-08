b8064f987b81555f1fee4a2aef6477aaf86a730a
KabiPriya <kabipriya.t@gmail.com>
2023-10-08 22:34:51 +0530

    func

---
+++ app/src/components/input.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Input({ label, name, type, placeholder }) {
+  return (
+    <div>
+      <label>
+        {label}
+        <input
+          name={name}
+          type={type}
+          placeholder={placeholder}
+        />
+      </label>
+    </div>
+  );
+}
+
+export default Input;
+//
